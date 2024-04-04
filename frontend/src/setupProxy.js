d3669597cb39348df68de53031bacc956e0ffa33
Libotte Dorian <d.libotte78@gmail.com>
2024-04-04 12:32:24 +0200

    Deploiement15

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,9 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+import { createProxyMiddleware } from 'http-proxy-middleware';
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000', // Adresse du serveur distant
+            target: 'https://projet-final-seven.vercel.app/', // Adresse du serveur distant
             changeOrigin: true, // Change l'origine de la requête pour éviter les problèmes CORS
         })
     );
