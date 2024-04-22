647c82093211776b1d0a64244196bd9a3682325e
Libotte Dorian <d.libotte78@gmail.com>
2024-04-22 15:14:27 +0200

    Responsive16

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -60,7 +60,7 @@ const Header = () => {
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         {/* Liens de navigation */}
-                        <Nav className='ml-auto'>
+                        <Nav className='navClass direction-column'>
                             {/* Lien vers le panier */}
                             <LinkContainer to='/cart'>
                                 <Nav.Link><i className='fas fa-shopping-cart'></i>Panier</Nav.Link>
